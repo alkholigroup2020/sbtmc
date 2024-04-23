ce3b1077e7c696f1612396f3a1e0ab58363eafd1
Fawzy Mohamed <fmohamed@alkholi.com>
2024-04-23 10:25:05 +0300

    facility-management home page - anout-us section done

---
+++ src/lib/facility-management/home/AboutUs.svelte
@@ -0,0 +1,59 @@
+<script lang="ts">
+	import { t } from '$lib/i18n/translator';
+	import { currentAppLang } from '$lib/stores';
+</script>
+
+<section>
+	<div class="container mx-auto py-12 md:py-16 xl:py-20">
+		<!-- title -->
+		<div class="flex flex-col items-center">
+			<div class="w-fit">
+				<h2
+					class="text-xl lg:text-3xl xl:text-4xl font-extrabold uppercase {$currentAppLang === 'en'
+						? 'pb-1'
+						: 'pb-3'}"
+				>
+					{$t('about.title')}
+				</h2>
+			</div>
+			<div class="{$currentAppLang === 'en' ? 'w-28' : 'w-20'} h-[3px] bg-primary-700"></div>
+		</div>
+		<!-- text -->
+		<div class="px-5 sm:px-8 md:px-28 xl:px-[15%] text-justify py-8 space-y-5">
+			<p class="text-base lg:text-lg">
+				{$t('about.p1')}
+			</p>
+			<p class="text-base lg:text-lg">
+				{$t('about.p2')}
+			</p>
+			<ul class="list-none space-y-2 text-base lg:text-lg">
+				<li>{$t('about.li1')}</li>
+				<li>{$t('about.li2')}</li>
+				<li>{$t('about.li3')}</li>
+				<li>{$t('about.li4')}</li>
+				<li>{$t('about.li5')}</li>
+				<li>{$t('about.li6')}</li>
+			</ul>
+		</div>
+		<!-- read more btn -->
+		<div class="flex justify-center py-2">
+			<button class="btn bg-primary-700 rounded-none text-white text-base lg:text-lg py-3">
+				<span>
+					{$t('about.more')}
+				</span>
+				<span
+					><svg
+						viewBox="0 0 1024 1024"
+						class="icon w-3 md:w-4 h-3 md:h-4 -rotate-90"
+						version="1.1"
+						xmlns="http://www.w3.org/2000/svg"
+						><path
+							d="M903.232 256l56.768 50.432L512 768 64 306.432 120.768 256 512 659.072z"
+							fill="#FFF"
+						/></svg
+					>
+				</span>
+			</button>
+		</div>
+	</div>
+</section>
--- src/lib/i18n/en/fm/home-page.ts
+++ src/lib/i18n/en/fm/home-page.ts
@@ -1,4 +1,18 @@
 export default {
 	'home.title.f': 'Facility',
-	'home.title.m': 'Management'
+	'home.title.m': 'Management',
+	'about.title': 'ABOUT US',
+	'about.p1':
+		'The development plan carried out by Saudi government has significantly helped Saudi private sector to develop and prosper in the last 2 decades. Nevertheless; in the fifth 5-year plan, maintenance works have been immensely prioritized by the Saudi business sector following the accomplishment of an integrated infrastructure in the Kingdom.',
+	'about.p2':
+		'One of the main objectives of the company is to provide the highest level of services through a horizontal/ vertical integrated organization in order to maximize their response to client requests and create flexible methods in copying with the work requirements through their reliance on a number of factors, such as:',
+	'about.li1': 'Consolidation of all company efforts on specialty.',
+	'about.li2': 'Provision of specialized management staff.',
+	'about.li3': 'Provision of specialized technical staff with the highest levels of performance. ',
+	'about.li4':
+		'Organizing training courses to technical staff of the company so that they get familiarized with the latest developments in the scope of work. ',
+	'about.li5': 'Introduction of the latest scientific information in the scope of work. ',
+	'about.li6':
+		'Foundation of an optimal integration between state-of-the-art technology, labor and costs.',
+	'about.more': 'Read More'
 };
